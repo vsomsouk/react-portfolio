d5a9353106700cb4fd98e7e0ee65078c9736fce8
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-09-13 19:05:21 -0500

    server

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '/public');
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(publicPath, '/public/index.html'));
  });
- 
+
 app.listen(port, () => {
    console.log(`Server is up on port ${port}!`);
 });
